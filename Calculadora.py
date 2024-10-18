d2c5ba5d3258bc4dc48cdb89ef83cdde4b65e13c
Saulo <saulobf8@gmail.com>
2024-10-18 17:43:51 -0300

    Criando Base de inserimento de dados

--- Calculadora.py
+++ Calculadora.py
@@ -3,13 +3,24 @@ import tkinter as tk
 main = tk.Tk()
 main.title("Calculadora")
 
+def exibir_numero():
+    numero_tela = entrada_numero.get()
+    numero_mostrado.config(text=f"Numero Digitado: {numero_tela} ")
 
+entrada_numero = tk.Entry(main, font=("Arial", 25))
+entrada_numero.pack()
 
-Texto_1 = tk.Label(main, text="DIGITAR NUMERO: ", font={"Arial", 30})
-Texto_1.pack()
-numero = int(input("DIGITAR UM NUMERO:"))
+numero_mostrado = tk.Label(main, text="", font=("Arial", 20))
+numero_mostrado.pack()
 
-Numero_tela = tk.Label(main,text=f"{numero}")
-Numero_tela.pack()
+botao_exibir_numero = tk.Button(main, text="Mostrar Texto", command=exibir_numero)
+botao_exibir_numero.pack()
+
+#Texto_1 = tk.Label(main, text="NUMERO: ", font={"Arial", 30})
+#Texto_1.pack()
+#numero = int(input("DIGITAR UM NUMERO:"))
+
+#Numero_tela = tk.Label(main,text=f"{numero}")
+#Numero_tela.pack()
 
 main.mainloop()
